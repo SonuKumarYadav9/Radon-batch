e24f2cf26eddbbdf9e6ef093ec5b339dab6de1ee
SonuKumarYadav9 <skumaryadav.man@gmail.com>
2022-07-20 12:33:36 +0530

    AWS promisses

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,21 +5,10 @@ const aws= require("aws-sdk")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-// s3 and cloud stodare
-//  step1: multer will be used to get access to the file in nodejs( from previous session learnings)
-//  step2:[BEST PRACTISE]:- always write s2 upload function separately- in a separate file/function..exptect it to take file as input and return the uploaded file as output
-// step3: aws-sdk install - as package
-// step4: Setupconfig for aws authenticcation- use code below as plugin keys that are given to you
-//  step5: build the uploadFile funciton for uploading file- use code below and edit what is marked HERE only
-
-
-//PROMISES:-
-// -you can never use await on callback..if you awaited something , then you can be sure it is within a promise
-// -how to write promise:- wrap your entire code inside: "return new Promise( function(resolve, reject) { "...and when error - return reject( err )..else when all ok and you have data, return resolve (data)
 
 aws.config.update({
     accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-    secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
+    secretAccessKey: "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",
     region: "ap-south-1"
 })
 
@@ -31,7 +20,7 @@ let uploadFile= async ( file) =>{
     var uploadParams= {
         ACL: "public-read",
         Bucket: "classroom-training-bucket",  //HERE
-        Key: "abc/" + file.originalname, //HERE 
+        Key: "bookCover/" + file.originalname, //HERE 
         Body: file.buffer
     }
 
@@ -74,3 +63,17 @@ router.post("/write-file-aws", async function(req, res){
 })
 
 module.exports = router;
+
+
+
+// s3 and cloud stodare
+//  step1: multer will be used to get access to the file in nodejs( from previous session learnings)
+//  step2:[BEST PRACTISE]:- always write s2 upload function separately- in a separate file/function..exptect it to take file as input and return the uploaded file as output
+// step3: aws-sdk install - as package
+// step4: Setupconfig for aws authenticcation- use code below as plugin keys that are given to you
+//  step5: build the uploadFile funciton for uploading file- use code below and edit what is marked HERE only
+
+
+//PROMISES:-
+// -you can never use await on callback..if you awaited something , then you can be sure it is within a promise
+// -how to write promise:- wrap your entire code inside: "return new Promise( function(resolve, reject) { "...and when error - return reject( err )..else when all ok and you have data, return resolve (data)
